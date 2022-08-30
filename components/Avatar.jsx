d27e487249487018ebfce7d2b41a938675cabf1c
sumarius56 <110302280+sumarius56@users.noreply.github.com>
2022-08-30 23:34:36 +0300

    Update Avatar.jsx

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
+
 
 const Avatar = ({url,className}) => {
   return (
-    <Image  className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+    <img  className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
      loading='lazy'
      src={url}
      alt='profile pic'
